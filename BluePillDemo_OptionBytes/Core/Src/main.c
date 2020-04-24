05671a6866cddf924cef2dc09011b6c2e3064033
John Blaiklock <miniwinwm@gmail.com>
2020-04-24 20:27:39 +0100

    Added option bytes example

---
+++ BluePillDemo_OptionBytes/Core/Src/main.c
@@ -0,0 +1,228 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  uint32_t ud;
+
+  // get existing user data bytes - note, it is actually a single byte each time but the API returns a uint32_t
+  ud = HAL_FLASHEx_OBGetUserData(OB_DATA_ADDRESS_DATA0);
+  ud = HAL_FLASHEx_OBGetUserData(OB_DATA_ADDRESS_DATA1);
+
+  // create and fill in a representation of the non-user data parts of the option bytes
+  FLASH_OBProgramInitTypeDef option_bytes;
+  HAL_FLASHEx_OBGetConfig(&option_bytes);
+
+  // set the bytes we want to write back, this must include the non-user data parts of the option bytes as they will be erased
+  option_bytes.OptionType = OPTIONBYTE_WRP | OPTIONBYTE_RDP | OPTIONBYTE_USER | OPTIONBYTE_DATA;
+
+  // set which user data byte to write and its value for the first user data byte
+  option_bytes.DATAAddress = OB_DATA_ADDRESS_DATA0;
+  option_bytes.DATAData = 0x55;
+
+  // unlock flash
+  HAL_FLASH_Unlock();
+  HAL_FLASH_OB_Unlock();
+
+  // write the option bytes (non-user data parts and 1 user data byte), this does an erase first internally when
+  // writing the non-user data option bytes
+  HAL_FLASHEx_OBProgram(&option_bytes);
+
+  // set which user data byte to write and its value for the second user data byte
+  option_bytes.DATAAddress = OB_DATA_ADDRESS_DATA1;
+  option_bytes.DATAData = 0xcc;
+
+  // no need to do another erase so no need to write all the non-user data option bytes and as
+  // only writing a user data byte this time choose just that and then write it
+  option_bytes.OptionType = OPTIONBYTE_DATA;
+  HAL_FLASHEx_OBProgram(&option_bytes);
+
+  // lock the flash again
+  HAL_FLASH_OB_Lock();
+  HAL_FLASH_Lock();
+
+  // re-read the user data 2 bytes again to check
+  ud = HAL_FLASHEx_OBGetUserData(OB_DATA_ADDRESS_DATA0);
+  ud = HAL_FLASHEx_OBGetUserData(OB_DATA_ADDRESS_DATA1);
+  (void)ud;
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ BluePillDemo_OptionBytes/readme.txt
@@ -0,0 +1,37 @@
+The STM32F103C8 has 16 bytes in flash that are extra to the main flash area. These are called 
+the option bytes. They are mainly for controlling aspects of the watchdog and read and write
+protection of the main flash. In addition however there are 2 bytes that are unused for these
+purposes and can be used by the programmer as non-volatile storage for any reason. This may
+be useful if the user only needs minimal storage and does not want to use a whole 1 kbyte of
+normal flash to do this.
+
+Although this can be done - it's not straightforward. Firstly the 2 user bytes cannot be
+erased on their own and being flash must be erased if any bits need setting from 0 to 1. Only
+the whole 16 byte area of the option bytes can be erased in one go. As you probably don't 
+want to change any of the other data in there you need to read and cache the existsing data,
+change the user data and write the whole lot back. The API is a bit strange as well in HAL,
+only 1 user data option byte can be written at a time so to write both it's a bit of a job...
+
+Read existing option bytes and cache in memory
+Unlock 2 flash areas
+Set the first user data byte in cached memory
+Do a combined erase/write back to flash
+Set the second user data byte in cached memory
+Do a write back to flash without erase
+Lock 2 flash areas.
+
+Reading is simpler, you just use the HAL API to read one of the 2 user data bytes at a time.
+
+Here's a further problem - the HAL options byte code is broken. It is fixed in this project. 
+The file that needs copying from this project is... 
+
+Drivers/STM32F1xxHAL_Driver/Src/stm32f1xx_hal_flash_ex.c
+
+If you don't do this then you will corrupt the option bytes in flash and have to use the 
+ST_LINK utility software to set it back to factory condition.
+
+Note: Debugging and stopping part way through this code or powering off in a similar way is
+likely to corrupt your option bytes. You will need to reset them to factor condition using
+ST-LINK utility if you do this.
+
+No schematic for this project.
